1fbf306b87ae95123e7e6b3d27159a9e794b9930
Ivana Zuber <ivanaz@counterparty.io>
2015-01-12 13:21:31 +0100

    Delete conf.py

--- conf.py
+++
@@ -1,9 +0,0 @@
-html_theme = 'default'
-if not on_rtd:
-    try:
-        import sphinx_rtd_theme
-        html_theme = 'sphinx_rtd_theme'
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-        html_theme_options = {'sticky_navigation' : True}
-    except ImportError:
-        pass
