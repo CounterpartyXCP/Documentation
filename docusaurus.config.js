4a066b99a851c2ef52464a4bf7ff36d33483e16a
Ouziel Slama <ouziel@gmail.com>
2024-03-21 20:29:47 +0100

    clean redirect plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,45 +115,4 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      '@docusaurus/plugin-client-redirects',
-      {
-        redirects: [
-          {
-            from: [
-              '/docs/basics/faq/smart-contracts/',
-              '/docs/basics/faq/lightning-network/',
-              '/docs/basics/support/',
-              '/docs/wallets/counterwallet',
-              '/docs/wallets/counterwallet-tutorials/getting-started',
-              '/docs/wallets/counterwallet-tutorials/create-addresses',
-              '/docs/wallets/counterwallet-tutorials/dex-trade',
-              '/docs/wallets/counterwallet-tutorials/multisig',
-              '/docs/wallets/counterwallet-tutorials/show-qrcode',
-              '/docs/wallets/counterwallet-tutorials/get-token-info',
-              '/docs/wallets/counterwallet-tutorials/create-token',
-              '/docs/wallets/counterwallet-tutorials/change-token-settings',
-              '/docs/wallets/counterwallet-tutorials/pay-distribution',
-              '/docs/wallets/counterwallet-tutorials/broadcast',
-              '/docs/wallets/counterwallet-tutorials/voting',
-              '/docs/wallets/counterwallet-tutorials/buy-sell',
-              '/docs/wallets/counterwallet-tutorials/create-armory-addresses',
-              '/docs/wallets/counterwallet-notes',
-              '/docs/advanced/federated-node/getting-started/',
-              '/docs/advanced/federated-node/pre-installation/',
-              '/docs/advanced/federated-node/installation/',
-              '/docs/advanced/federated-node/administration/',
-              '/docs/advanced/federated-node/counterwallet/',
-              '/docs/advanced/installation/bitcoin-core/',
-              '/docs/advanced/installation/windows/',
-              '/docs/develop/counterblock/api/',
-              '/docs/develop/counterblock/modules/',
-            ],
-            to: '/docs/',
-          },
-        ],
-      }
-    ]
-  ],
 };
